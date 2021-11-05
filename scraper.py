ae67f1f79612c1eef8ce937ae4cee2254067bb80
Deniz Ozturk <deniz.ozturk.1@outlook.com>
2021-11-05 20:06:31 +0000

    Duplicate URLs now removed from newsLinks array

--- scraper.py
+++ scraper.py
@@ -8,12 +8,19 @@ def getHeadline(targetURL): # gets headline of article
     cleanedHeadline = re.sub(' - BBC News', '', someSoup.find('title').string)
     return cleanedHeadline
 
+def checkDuplicate(array3D, targetURL): # check for duplicate URL in 3d arra
+    itemFound = False
+    for array in array3D:
+        if array[1] == targetURL:
+            itemFound = True
+    return itemFound             
+
 url = "https://www.bbc.co.uk/news"
 page = requests.get(url)
 soup = bs(page.content, 'html.parser')
 
 # GET ALL NEWS PAGE URLS & CORRESPONDING HEADLINE
-newsLinks = {}
+newsLinks = [] # will be 3D array
 for link in soup.find_all(class_ = 'gs-c-promo-heading'):
     href = str(link.get('href'))
     if href != "None":
@@ -21,13 +28,7 @@ for link in soup.find_all(class_ = 'gs-c-promo-heading'):
             href = 'https://www.bbc.co.uk' + href
             if (href.startswith('https://www.bbc.co.uk/news/') == True):
                 headline = getHeadline(href)
-                newsLinks.update({href : headline})
-
-print (newsLinks)
-# TODO: remove duplicates from newsLinks dictionary
-
-""" newsLinksDict = {}
-for link in newsLinks:
-    somePage = requests.get(link)
-    someSoup = bs(somePage.content, 'html.parser')
-    print (someSoup.find('title')) """
+                if checkDuplicate(newsLinks, href) == False:
+                    tempArray = [headline, href]
+                    newsLinks.append(tempArray)
+                    print (tempArray)
