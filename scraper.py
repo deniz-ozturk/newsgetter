c3394c2483786ab66020ae7b90104ecab6a41b84
Deniz Ozturk <deniz.ozturk.1@outlook.com>
2021-11-05 20:47:44 +0000

    Begun making menu and getArticle function

--- scraper.py
+++ scraper.py
@@ -8,6 +8,12 @@ def getHeadline(targetURL): # gets headline of article
     cleanedHeadline = re.sub(' - BBC News', '', someSoup.find('title').string)
     return cleanedHeadline
 
+def getArticle(targetURL):
+    somePage = requests.get(href)
+    someSoup = someSoup = bs(somePage.content, 'html.parser')
+    # TODO: needs to also get titles from the article as well
+    #articleContent = someSoup.find_all('')
+
 def checkDuplicate(array3D, targetURL): # check for duplicate URL in 3d arra
     itemFound = False
     for array in array3D:
@@ -31,4 +37,22 @@ for link in soup.find_all(class_ = 'gs-c-promo-heading'):
                 if checkDuplicate(newsLinks, href) == False:
                     tempArray = [headline, href]
                     newsLinks.append(tempArray)
-                    print (tempArray)
+
+# DISPLAY NEWS TO USER
+print ("BBC News articles: \n")
+counter = 0
+for array in newsLinks:
+    counter += 1
+    print (str(counter) + ")", array[0])
+
+while True:
+    chosenValue = int(input("\nSelect an article number: "))
+    articleName = newsLinks[chosenValue - 1][0]
+    print ("Selected '" + articleName + "', Select an action from below:")
+    chosenAction = int(input("1 to read article, 2 to save article, 3 to select a different article: "))
+    if chosenAction == 1:
+        pass
+    else:
+        print ("\n test")
+
+# TODO: don't display articles that have only a video and no text
